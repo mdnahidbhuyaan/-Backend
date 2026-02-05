709e794e44e05a0a96b242e7cfd51fac22d27251
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2026-02-05 23:38:11 +0600

    Rest api setup

--- src/app.js
+++ src/app.js
@@ -7,10 +7,35 @@ const notes = []
 app.post("/notes", (req, res) => {
  notes.push(req.body)
  res.status(201).json({
-
     message: "Note added successfully"
  })
 })
+
+app.get("/notes",(req,res)=>{
+res.status(200).json({
+   message:"Notes fetched successfully",
+   notes:notes
+})
+})
+
+app.delete("/notes/:index", (req, res) => {
+
+   const index = req.params.index
+   delete notes[index]
+   res.status(200).json({
+      message: "Note deleted successfully"
+   })
+})
+
+app.patch("/notes/:index",(req,res)=>{
+   const index = req.params.index
+   const description = req.body.description
+   notes[index].description = description
+   res.status(200).json({
+      message:"Note updated successfully"
+   })
+})
+
 
 
 module.exports = app
